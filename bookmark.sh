1a30b08aab53a39b5c3403606d81d6ddc379449d
noah-conway <nconway@posteo.net>
2026-05-18 22:02:50 -0700

    added picker/retrieval functionality to bookmark script

--- bookmark.sh
+++ bookmark.sh
@@ -1,28 +1,53 @@
 #!/bin/sh
 
-BKMK_FILE="${BOOKMARKS_FILE:-"$HOME/bmk/.bookmarks"}"
+EXIT_CODE=0
+BKMK_FILE="${BOOKMARKS_FILE:-"$HOME/.bookmarks.csv"}"
+PICKERCMD="fzf"
+BROWSERCMD="${BROWSER:-"firefox"}"
 
 add_bookmark () {
   local URL="$1"
   local TITLE="$(curl -sL "$URL" | grep -oP '(?<=<title>).*?(?=</title>)' | tr -d ",")"
-  local BKMK_LINE="$TITLE,$URL"
+  local BKMK_LINE="$TITLE,$URL,$(date +%s)"
+  local msg="Added $URL to bookmarks file"
 
-  mkdir -p "$(dirname $BKMK_FILE)"
-  touch "$BKMK_FILE"
+  if [ ! -f "$BKMK_FILE" ]; then
+    mkdir -p "$(dirname $BKMK_FILE)"
+    touch "$BKMK_FILE"
+  fi
+
+  grep -q "$URL" "$BKMK_FILE" && 
+  msg="$URL already exists in bookmarks file";EXIT_CODE=1 ||
   echo "$BKMK_LINE" >> "$BKMK_FILE"
+
+  notify-send "$msg"
 }
 
 
-while getopts ":a:" opt; do
+while getopts ":a:p:b:" opt; do
   case "$opt" in
     a)
       add_bookmark "$OPTARG"
+      return $EXIT_CODE
+      ;;
+    p) #specify picker
+      PICKERCMD="$OPTARG"
+      ;;
+    b)
+      BROWSERCMD="$OPTARG"
       ;;
     \?)
       echo "Error: Invalid option $OPTARG"
       # invalid arg
+      ;;
   esac
 done
+
+URL="$(cut -d, -f1,2 "$BKMK_FILE" | $PICKERCMD | cut -d, -f2)" &&
+$BROWSERCMD $URL
+
+
+exit $EXIT_CODE
 
 #URL=$1
 #TITLE="$(curl -sL "$URL" | grep -oP '(?<=<title>).*?(?=</title>)')"
